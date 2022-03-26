767b99fb77e20fbe59abd5996a56b16207a98f81
Berger <Bergeredvard6@gmail.com>
2022-03-26 19:30:18 +0100

    Gjort endringer for testing av hosting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import hent_info
-
+import csv
 kundeNavn = "Knut Arild Hareide"
 knut = {"numberOfHits":2,"hits":[{"score":0.0003451437393358565,"id":"evpo-ff10bc5a-4aad-4895-b886-fb88e598ed57","schema":"Person","name":"Knut Arild Hareide","aliases":"Hareide, Knut Arild","birth_date":"1972-11-23","countries":"no","identifiers":"Q1350399","sanctions":"","phones":"","emails":"","dataset":"Every Politician","last_seen":"2021-07-26 11:55:45","first_seen":"2021-10-07 03:02:59"},{"score":0.01857804455091699,"id":"us-cia-norway-knut-arild-hareide-min-of-transportation-communications","schema":"Person","name":"Knut Arild HAREIDE","aliases":"","birth_date":"","countries":"no","identifiers":"","sanctions":"","phones":"","emails":"","dataset":"CIA World Leaders","last_seen":"2021-07-26 11:55:45","first_seen":"2021-10-07 03:02:59"}]}
 
@@ -19,14 +19,22 @@ def hentDataApi(kundeNavn):
     return data[:-1] + '.'
 
 def hentDataCsv(kundeNavn):
+    #Løper gjennom csv filen og leter etter personen
+    #Noterer seg alle databasene personen er funnet i
     kundeNavn = kundeNavn.upper()
     flags = 0
-    data = []
+    data = ''
     with open('pep.csv', encoding="utf-8") as csv_file:
-        for row in csv_file:
-            if kundeNavn in row.upper():
-                data.append(eval("[" + row + "]")[11])
-    return data
+        read_csv = csv.DictReader(csv_file)
+        for row in read_csv:
+            if kundeNavn in row["name"].upper():
+                data += ' ' + row['dataset'] + ','
+                flags += 1
+    if data == '':
+        return "No data in database."
+    else:
+        data = "Antall treff: " + str(flags) + ". Fra" + data
+        return data[:-1] + '.'
 
 
 def main(getInfo):
